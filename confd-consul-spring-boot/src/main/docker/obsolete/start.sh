121134a7ee195bfa60086eb581e90540def26cb1
frank <frank.sprich@gmail.com>
2018-01-29 07:37:26 -0800

    Deleted the obsolete

--- confd-consul-spring-boot/src/main/docker/obsolete/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## http://www.mricho.com/confd-and-docker-separating-config-and-code-for-containers/
-set -e
-
-confd -onetime -backend env
-
-echo "Starting SpringBoot app: confd-example"
-
-exec java -jar $ARTIFACT
